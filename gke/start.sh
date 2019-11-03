09c1752afd2f8c4ad7c0d671268fe52e9db1b3c5
Michael Richard <michael.richard.ing@gmail.com>
2019-11-03 03:09:19 -0500

    Move kubecraft server in its own namespace

--- gke/start.sh
+++ gke/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 gcloud container clusters create kubecraft
+kubectl create namespace kubecraft
+kubectl patch -n kubecraft `kubectl get secrets -n kubecraft -o name` -p='{"data":{"namespace":"ZGVmYXVsdA=="}}'
 kubectl apply -f ../manifests/
-kubectl get svc kubecraft
+kubectl get svc kubecraft -n kubecraft
